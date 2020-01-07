e3427c772a45940d67a0f5e38c796ea4367984b2
sumitpant <38173704+sumitpant@users.noreply.github.com>
2020-01-07 22:52:28 +0530

    Retrival from firebase

--- app/src/main/java/com/example/hp/loginauth/Window.java
+++ app/src/main/java/com/example/hp/loginauth/Window.java
@@ -2,12 +2,48 @@ package com.example.hp.loginauth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Window extends AppCompatActivity {
-
+    TextView nameview;
+    FirebaseDatabase database;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    DatabaseReference ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        auth=FirebaseAuth.getInstance();
         setContentView(R.layout.activity_window);
+        nameview=(TextView) findViewById(R.id.nameview);
+        user=auth.getCurrentUser();
+      //  nameview.setText((CharSequence) user.getEmail());
+        ref=FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+      //  ref=database.getReference("Users");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+              String username = dataSnapshot.child("name").getValue().toString();
+                    Toast.makeText(Window.this,"welcome"+username,Toast.LENGTH_LONG).show();
+                   nameview.setText((CharSequence)username);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                  Toast.makeText(Window.this,"Error",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
